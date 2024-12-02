a5647053b4e6e71a9aec9e1b248ce55c1801a617
Victor <victormartin96@outlook.com>
2024-12-02 21:18:47 +0000

    Make solution cooler and extra unreadable

--- day2/sol2.py
+++ day2/sol2.py
@@ -2,16 +2,12 @@ import sys
 
 read_line = lambda line : list(map(int, line.split()))
 check_increasing = lambda L, R: all(1 <= r - l <= 3 for (l, r) in zip(L, R))
-call_adjacent = lambda func, lst : func(lst[:-1], lst[1:])
-call_adjacent2 = lambda func, lst : func(lst[1:], lst[:-1])
-
-def check_almost_increasing(line):
-    ind = next((i for (i, (l, r)) in enumerate(zip(line, line[1:])) if not(1 <= r - l <= 3)), None)
-    return ind is None or call_adjacent(check_increasing, line[:ind] + line[ind+1:]) or call_adjacent(check_increasing, line[:ind + 1] + line[ind+2:])
-
-def check_almost_decreasing(line):
-    ind = next((i for (i, (l, r)) in enumerate(zip(line, line[1:])) if not(1 <= l - r <= 3)), None)
-    return ind is None or call_adjacent2(check_increasing, line[:ind] + line[ind+1:]) or call_adjacent2(check_increasing, line[:ind + 1] + line[ind+2:])
-
-is_safe = lambda line: check_almost_increasing(line) or check_almost_decreasing(line)
+adj_callback = lambda func, lst, ordered : \
+    func(lst[:-1] if ordered else lst[1:], lst[1:] if ordered else lst[:-1])
+def check_almost_monotonous(line, ordered):
+    ind = next((i for (i, (l, r)) in enumerate(adj_callback(zip, line, ordered))
+                    if not(1 <= r - l <= 3)), len(line))
+    return any(adj_callback(check_increasing, line[:ind+offset] + line[ind+offset+1:], ordered)
+                    for offset in [0, 1])
+is_safe = lambda line: any(check_almost_monotonous(line, ordered) for ordered in [True, False])
 print(sum(is_safe(line) for line in map(read_line, sys.stdin)))

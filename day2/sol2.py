1aa2de43234491b4f3ee923e3e379ce4e53973fc
Victor <victormartin96@outlook.com>
2024-12-03 21:46:06 +0000

    Improve solution (kinda)

--- day2/sol2.py
+++ day2/sol2.py
@@ -1,13 +1,13 @@
 import sys
 
+check_seq_is_increasing = lambda lst: all(1 <= r - l <= 3 for (l, r) in zip(lst, lst[1:]))
+find_next_fail_pos = lambda lst: \
+    next((i for (i, (l, r)) in enumerate(zip(lst, lst[1:])) if not(1 <= r - l <= 3)), None)
+check_seq_is_almost_increasing_from_failed_index = lambda lst, ind: \
+    ind is None or any(find_next_fail_pos(lst[:ind+offset] + lst[ind+offset+1:]) is None for offset in [0, 1])
+check_seq_is_almost_increasing = lambda lst : check_seq_is_almost_increasing_from_failed_index(
+                                                            lst, find_next_fail_pos(lst))
+
 read_line = lambda line : list(map(int, line.split()))
-check_increasing = lambda L, R: all(1 <= r - l <= 3 for (l, r) in zip(L, R))
-adj_callback = lambda func, lst, ordered : \
-    func(lst[:-1] if ordered else lst[1:], lst[1:] if ordered else lst[:-1])
-def check_almost_monotonous(line, ordered):
-    ind = next((i for (i, (l, r)) in enumerate(adj_callback(zip, line, ordered))
-                    if not(1 <= r - l <= 3)), len(line))
-    return any(adj_callback(check_increasing, line[:ind+offset] + line[ind+offset+1:], ordered)
-                    for offset in [0, 1])
-is_safe = lambda line: any(check_almost_monotonous(line, ordered) for ordered in [True, False])
-print(sum(is_safe(line) for line in map(read_line, sys.stdin)))
+print(sum(check_seq_is_almost_increasing(line) or check_seq_is_almost_increasing(line[::-1])
+            for line in map(read_line, sys.stdin)))

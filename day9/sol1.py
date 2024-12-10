ca36f7f658e4ba464c70e6d1a9b3a3405be89a96
Victor <victormartin96@outlook.com>
2024-12-10 20:56:13 +0000

    Improve code of day 9

--- day9/sol1.py
+++ day9/sol1.py
@@ -1,20 +1,20 @@
 import itertools
-from typing import List, Union
+from typing import List, Union, Iterable, Tuple, Callable, Generator
 
 # Build chain from input
 chain : List[Union[str, int]] = \
     [i//2 if i&1 == 0 else '.' for i, c in enumerate(input()) for _ in range(int(c))]
 
 # Reversed iterator on the chain, enumerated
-rev_iter = iter((i, val) for i, val in list(enumerate(chain))[::-1] if val != '.')
+rev_iter: Iterable[Tuple[int, int]] = iter((i, val) for i, val in list(enumerate(chain))[::-1] if val != '.')
 
 # Get next relevant number in the chain. If the current position has an integer, return it,
 # Otherwise return the next integer as seen from the end
-get_next = lambda j, val : (j, val) if isinstance(val, int) else next(rev_iter)
+get_next: Callable[[int, int], Tuple[int, int]] = lambda j, val : (j, val) if isinstance(val, int) else next(rev_iter)
 
 # Generate the numbers of the final chain, multiplied by its index
-def iterations():
-    j = len(chain)
+def iterations() -> Generator[int, None, None]:
+    j: int = len(chain)
     for i, val in itertools.takewhile(lambda P: P[0] < j, enumerate(chain)):
         j, val2 = get_next(j, val)
         yield i*val2
--- day9/sol2.py
+++ day9/sol2.py
@@ -1,22 +1,21 @@
-import itertools
 from typing import List, Union
 
-# Read input
-inp = [(i//2 if i&1 == 0 else '.', int(c)) for i, c in enumerate(input())]
+# Transform each input character to its value and repetition count
+inp: List[Union[int, str]] = [(i//2 if i&1 == 0 else '.', int(c)) for i, c in enumerate(input())]
 
+# For each value in the input (for larger to smaller), try to replace in its new position, and update the list
 for r_val in list(range(len(inp)//2 + 1))[::-1]:
-    for ind, (rv, rr) in enumerate(inp):
-        if rv == r_val:
-            r_reps, rhs_ind = rr, ind
-    if r_val == '.': continue
-    for lhs_ind, (l_val, l_reps) in enumerate(inp):
-        if lhs_ind >= rhs_ind:
-            break
-        if l_val != '.': continue
-        if l_reps >= r_reps:
-            change = True
-            inp = inp[:lhs_ind] + [(r_val, r_reps), ('.', l_reps - r_reps)] + inp[lhs_ind + 1:rhs_ind] + [('.', r_reps)] + inp[rhs_ind + 1:]
-            break
+    # Find current value index and repetition counter
+    rhs_ind, r_reps = next((ind, rr) for ind, (rv, rr) in enumerate(inp) if rv == r_val)
 
-new_list = list(0 if val == '.' else val for i, (val, reps) in enumerate(inp) for _ in range(reps))
-print(sum(i*x for i, x in enumerate(new_list)))
+    # Find next space for the value, None if there is none.
+    # Only consider empty spaces, among them get the first with at least r_reps spaces
+    lhs_ind, l_reps = next(((lhs_ind, l_reps) for lhs_ind, (_, l_reps) in
+                                filter(lambda T: T[1][0] == '.', enumerate(inp[:rhs_ind])) if l_reps >= r_reps), (None, None))
+
+    # Update the list by moving the right index to the left
+    if lhs_ind is not None:
+        inp = inp[:lhs_ind] + [(r_val, r_reps), ('.', l_reps - r_reps)] + inp[lhs_ind + 1:rhs_ind] + [('.', r_reps)] + inp[rhs_ind + 1:]
+
+# Expand the summary list, and perform the final computation
+print(sum(i*x for i, x in enumerate(0 if val == '.' else val for val, reps in inp for _ in range(reps))))

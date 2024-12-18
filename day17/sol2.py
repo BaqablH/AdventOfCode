f9ae7c65c388237e7cdac8d374f86b17bdd6b5ab
Victor <victormartin96@outlook.com>
2024-12-18 19:14:00 +0000

    Improve solution 2 day 16

--- day17/sol2.py
+++ day17/sol2.py
@@ -1,23 +1,17 @@
 from itertools import product
 
+# Read registers
 registers = list(int(input().split()[-1].strip()) for _ in range(3))
 
 # Read empty line
 input()
 
 # Get program (in binary)
-program = list(map(int, input().split(": ")[1].split(',')))
-binary_program = list(map(int, bin(sum(x << (3*i) for i, x in enumerate(program))).removeprefix('0b')))
-binary_program = binary_program[::-1] + [0]*(3*len(program) - len(binary_program))
+binary_program = list(map(int, "".join(
+    bin(8 | int(x)).removeprefix('0b1')[::-1] for x in input().split(": ")[1].split(','))))
 
-# Array of the bits of candidate initial register A
-A = [None]*(3 * len(program))
-
-# Get n-th element of A. If unset, default to array Q
-def get(Q, n):
-    if n >= len(A): return 0
-    if A[n] is None: return Q[n % 3]
-    return A[n]
+# Array of the bits of candidate initial register A (with extra padding)
+A = [None]*len(binary_program) + [0]*9
 
 answer = None
 def backtrack(i = 15):
@@ -29,30 +23,21 @@ def backtrack(i = 15):
     
     # Answer found
     if i == -1:
-        answer = sum(x << i for i, x in enumerate(A[::1]))
+        answer = sum(x << i for i, x in enumerate(A))
         return 
     
-    # Iterate over the possible 8 next triplets of bits
-    for P in product([0, 1], repeat=3):
-        # Unset A on the given bits
-        for j in range(3):
-            A[3*i + j] = None
+    # Iterate over the possible 8 next triplets of bits, ordered by most significant bit
+    for c, b, a in product([0, 1], repeat=3):
+        # Set A for the new elements
+        for j, x in enumerate([a, b, c]):
+            A[3*i + j] = x
 
-        # Order by most significant bit
-        c, b, a = P
-
-        # Get shift part in our equation
+        # Get shift part in our equation and set conditions
         shift = ((c^1) << 2) ^ (b << 1) ^ (a^1)
-
-        # A is ordered the other way around
-        Q = [a, b, c]
+        kth_condition_satisfied = lambda k : binary_program[k] == A[k] ^ int(k%3 != 2) ^ A[k + shift]
 
         # If all bits satisfy the bit equation, set the specific A bits and continue the backtracking
-        if all(
-            binary_program[3*i + j] == get(Q, 3*i + j) ^ int(j != 2) ^ get(Q, 3*i + j + shift) for j in range(3)
-        ):
-            for j in range(3):
-                A[3*i + j] = Q[j]
+        if all(kth_condition_satisfied(3*i + j) for j in range(3)):
             backtrack(i - 1)
 
 # Start backtracking
